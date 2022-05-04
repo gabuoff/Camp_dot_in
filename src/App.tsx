820d50793a84a35e38be256a28ff8619b7376ead
Gabriel Melo <gabrielmeloc@gmail.com>
2022-05-04 17:18:44 -0300

    init css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import './styles/App.css'
 import { Login } from './pages/Login'
+
+import bgImg from './assets/images/side-image.jpg'
+import './styles/App.css'
 
 export function App() {
  
   return (
-   <Login />
+    <>
+    <div className="container">
+      <main>
+        <div className="main-content">
+         <Login /> 
+         </div>
+      </main>
+   
+   <figure>
+     <img  src={bgImg} alt='background img' />
+   </figure>
+   </div>
+    </>
   )
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import '../styles/login.css'
+import React, { useState } from "react";
 
-export function Login(){
-    return (
-        <>
-            <div className="container">
+import loginTitle from "../assets/images/log-in.svg";
+import "../styles/Login.css";
 
-            </div>
-        </>
-    )
+export function Login() {
+  return (
+    <form>
+      <div className="title-login">
+        <picture>
+          <img src={loginTitle} alt="login title img" />
+        </picture>
+        <h1>Faça seu login</h1>
+      </div>
+
+      <span>Entre com suas informações de cadastro</span>
+      <fieldset className="">
+        <label>E-mail</label>
+        <input type="email" name="uname" placeholder="Digite seu e-mail" />
+      </fieldset>
+
+      <fieldset className="">
+        <label>Senha </label>
+        <input type="password" name="pass" placeholder="Digite sua senha" />
+      </fieldset>
+
+      <div className="checkbox">
+        <input type="checkbox" name="remember" id="remember" />
+        <label>Lembre-me</label>
+      </div>
+    </form>
+  );
 }
