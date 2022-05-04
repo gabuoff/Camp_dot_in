d578616c63fe675c871ef4ea68c1b1ee43c04d51
Gabriel Melo <gabrielmeloc@gmail.com>
2022-05-04 14:38:25 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/App.css'
+import { Login } from './pages/Login'
+
+export function App() {
+ 
+  return (
+   <Login />
+  )
+}
+
+
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/login.css'
+
+export function Login(){
+    return (
+        <>
+            <div className="container">
+
+            </div>
+        </>
+    )
+}
